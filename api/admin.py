f143411bb3cf0ffd5b34cecc429d1f9c32153fd4
Rajabboy <rajrajrajradju@gmail.com>
2025-07-25 17:27:46 +0500

    finished admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone_number', 'invite_code', 'activated_invite_code')
+    list_display_links = ('phone_number',)
+    search_fields = ('invite_code', 'phone_number')
+
+
+admin.site.register(User, UserAdmin)
+
--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import VerificationCode
 
-# Register your models here.
+
+class VerificationCodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'phone_number', 'valid_for')
+    list_display_links = ('phone_number',)
+    search_fields = ('code', 'phone_number')
+
+
+admin.site.register(VerificationCode, VerificationCodeAdmin)
